fb3fd1265783a474abebee8b622d9ba964f408be
jensbech <jens@bechsor.no>
2023-12-27 10:27:39 +0000

    work

--- bot.py
+++ bot.py
@@ -1,49 +1,44 @@
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import os
 import aiohttp
+import os
+
 
 load_dotenv()
 
 
-class DiscordBot:
-    def __init__(self, command_prefix, intents):
-        self.bot = commands.Bot(command_prefix=command_prefix, intents=intents)
+class BookStackAPI(commands.Cog):
+    def __init__(self, bot, base_url):
+        self.bot = bot
+        self.base_url = base_url
+        self.api_id = os.getenv('BOOKSTACK_API_ID')
+        self.api_key = os.getenv('BOOKSTACK_API_KEY')
+        self.auth_header = {'Authorization': f'Token {
+            self.api_id}:{self.api_key}'
+        }
 
+    @commands.Cog.listener()
     async def on_ready(self):
         print(f'Logged in as {self.bot.user.name}')
 
-    async def get_data_from_api(self):
+    @commands.command(name="books", description="Retrieve books data")
+    async def books(self, ctx: commands.context):
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://wiki.boredgods.no/api/books') as response:
+            async with session.get(self.base_url + "/books", headers=self.auth_header) as response:
                 if response.status == 200:
                     data = await response.json()
-                    return data
+                    await ctx.send(f"Books data: {data}")
                 else:
                     print(f"Failed to retrieve data: {response.status}")
-                    return None
+                    await ctx.send("Failed to retrieve books data.")
 
-    @commands.command(name='getbooks')  # Define the command
-    async def get_books(self, ctx):
-        print("get_books called")
-        data = await self.get_data_from_api()
-        if data:
-            await ctx.send(f"Books data: {data}")
-        else:
-            await ctx.send("Failed to retrieve books data.")
-
-    def run(self):
-        self.bot.add_command(self.get_books)
-
-        @self.bot.event
-        async def on_ready():
-            await self.on_ready()
-
-        self.bot.run(os.getenv('BOT_TOKEN'))
-
-
-if __name__ == "__main__":
-    intents = discord.Intents.all()
-    my_bot = DiscordBot(command_prefix='/', intents=intents)
-    my_bot.run()
+    @commands.command()
+    async def search(self, ctx: commands.context, query: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.base_url + f"/search?query={query}", headers=self.auth_header) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    await ctx.send(f"Search data: {data}")
+                else:
+                    print(f"Failed to retrieve data: {response.status}")
+                    await ctx.send("Failed to retrieve search data.")
--- program.py
+++ program.py
@@ -1,6 +1,20 @@
 import discord
-from bot import DiscordBot
+from discord.ext import commands
+import os
+from bot import BookStackAPI
+import asyncio
 
-bot = DiscordBot(command_prefix='/', intents=discord.Intents.all())
 
-bot.run()
+def custom_prefix(bot, message):
+    return commands.when_mentioned_or()(bot, message)
+
+
+async def main():
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix=custom_prefix, intents=intents)
+
+    await bot.add_cog(BookStackAPI(bot, "https://wiki.boredgods.no/api"))
+
+    await bot.start(os.getenv('BOT_TOKEN'))
+
+asyncio.run(main())
