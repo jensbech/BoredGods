952bba7cdd2bef2f0c5077d595c87df2d6cd8612
jensbech <jens@bechsor.no>
2023-12-27 20:04:27 +0000

    finish

--- program.py
+++ program.py
@@ -1,17 +1,13 @@
 import discord
 from bookstack.apiclient import BookStackAPIClient
 import os
+
 from commands.roll import roll
 from commands.search import search
-import aiohttp
+from commands.weather import weather
+
 client = BookStackAPIClient(intents=discord.Intents.default())
 baseurl = os.getenv("BASE_URL")
-
-valid_categories = ["classes", "conditions",
-                    "equipment", "feats",
-                    "languages", "magic-items", "magic-schools", "monsters", "proficiencies",
-                    "races", "skills",
-                    "weapons", "spells"]
 
 
 @client.event
@@ -29,52 +25,19 @@ async def roll_command(interaction: discord.Interaction, dice: str):
     await roll(interaction, dice)
 
 
-@client.tree.command(name="regler")
-async def lookup_command(interaction: discord.Interaction, kategori: str, navn: str):
-
-    if kategori not in valid_categories:
-        error_message = f"Ukjent kategori. Bruk en av de følgende: **{
-            ', '.join(valid_categories)}**."
-        await interaction.response.send_message(error_message)
-        return
-
-    resolvedName = navn.replace(" ", "-").lower()
-
-    url = f"https://www.dnd5eapi.co/api/{kategori}/{resolvedName}"
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                data = await response.json()
-
-                print(data)
-
-                name = data['name']
-                desc = data['desc'][0]
-
-                message = f"\n**Name:** {name}\n"
-                message += f"**Description:** {desc}"
-
-                await interaction.response.send_message(message)
-            else:
-                error_message = "Regel ikke funnet, eller en feil. Sjekk navnet og prøv igjen!"
-                await interaction.response.send_message(error_message)
+@client.tree.command(name="værmelding")
+async def weather_command(interaction: discord.Interaction):
+    await weather(interaction)
 
 
 @client.tree.command(name="hjelp")
 async def help_command(interaction: discord.Interaction):
-    category_list = ", ".join(
-        [f"`{category}`" for category in valid_categories])
-
     messages = [
         "Følgende kommandoer er tilgjengelige:",
         "`/søk soskni` - Søk i Wiki",
-        "`/rull d20`- Rull to win",
-        "`/regler`",
-        "\nTilgjengelige kategorier for `/regler <kategori> <navn>`:",
-        category_list
+        "`/rull d20` / `/rull 2d6-2` - Rull to win",
+        "`/vær` - Sjekk været i Stone-upon-hill",
     ]
-
     help_message = "\n".join(messages)
     await interaction.response.send_message(help_message)
 
