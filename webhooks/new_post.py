4051015a2b5b128faa5ade2fc0571270acff0bd6
jensbech <jens@bechsor.no>
2023-12-29 15:01:28 +0000

    fix small error

--- webhooks/new_post.py
+++ webhooks/new_post.py
@@ -30,13 +30,10 @@ def create_app(discord_client):
 
         discord_message = f"A new Wiki page **{page_name}** was just created by {triggered_by}\n{page_url}"
 
-        # Assuming you have the channel ID as an environment variable
-        channel_id = int(os.getenv(os.getenv("DISCORD_CHANNEL_ID")))
+        channel_id = os.getenv(os.getenv("DISCORD_CHANNEL_ID"))
 
-        # Get the Discord channel object
         channel = client.get_channel(channel_id)
 
-        # If the channel was found, send the message
         if channel:
             asyncio.run_coroutine_threadsafe(
                 channel.send(discord_message),
